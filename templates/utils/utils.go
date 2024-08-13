d91793ffe86e8bfab0b26ca87162a89e6cb87a0d
aaronknudtson <aaron.knudtson@outlook.com>
2024-08-12 22:15:39 -0400

    added isauthenticated tool for template utils

---
+++ templates/utils/utils.go
@@ -0,0 +1,12 @@
+package utils
+
+import (
+	"context"
+
+    ctx "my_project/pkg/context"
+)
+
+func IsAuthenticated(c context.Context) bool {
+	myCtx, ok := c.Value("myCtx").(*ctx.WebCtx)
+	return ok && myCtx != nil
+}
